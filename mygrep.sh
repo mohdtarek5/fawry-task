613fb3ab5b9109b2db3b2ff155689a29c8b67447
admin1 <admin1@admin1.myguest.virtualbox.org>
2025-04-28 15:01:23 +0300

    initial plan

---
+++ mygrep.sh
@@ -0,0 +1,27 @@
+#command should be able to search for strings inside text file
+#allow the use of commands like -v and -n and a combination of both
+#handle errors when no string is given with a prompt message
+
+
+#first variable = command
+#second variable = string check
+#third variable = filename
+
+#if 3rd variable = null 
+#set variable as 1st string
+#2nd variable as filename
+#else if 3 variables 1st is command 2nd is string 3rd filename
+#else error handle
+
+#verify -n, -v, -nv, -vn commands incase they are 3 arguments
+#store commands in separate variable
+#if less than 2 arguments, then exit
+#if valid
+#set line counter to 0
+#loop through text
+#match each word to variable
+#if word matching make a new variable
+#if matching store in new variable matchingWords
+#if not matching put in variable invertedWords
+
+#if there is time implement bonus task
